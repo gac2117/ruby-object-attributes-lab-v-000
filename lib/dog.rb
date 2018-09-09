72bd8d391e13f676cd43fbc3c9758e31c7e7850b
Grace Choi <oiograceoio@gmail.com>
2018-09-09 11:53:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,14 +2,11 @@ class Dog
   def name= (dog_name)
     this_dogs_name = dog_name)
   end
-  
+
   def name
     this_dogs_name
   end
 
-  def name= (new_name)
-    @name = new_name
-  end
 
   def breed
     @breed
